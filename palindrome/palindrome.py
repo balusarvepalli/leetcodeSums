e3ab89f5741532cfe2b9874fa118cb911bd19730
balu <balusarvepalli@gmail.com>
2022-07-15 18:31:17 +0530

    palindrome code

---
+++ palindrome/palindrome.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return(False)
+        reverse_x = 0
+        digit = 0
+        while(x // (10**digit) !=0):
+            reverse_x = (reverse_x * 10) + (x // (10**digit) % 10)         
+            digit +=1
+        return(x == reverse_x)
